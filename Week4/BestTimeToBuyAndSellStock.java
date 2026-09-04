2584d05b26cf8589ea64d30c3df06ee50f6a0b59
KavinMuralisankar <km2735@srmist.edu.in>
2026-09-05 02:42:01 +0530

    Add Week 4 practice problem 2

---
+++ Week4/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,16 @@
+public class BestTimeToBuyAndSellStock {
+    static int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) minPrice = prices[i];
+            else if (prices[i] - minPrice > maxProfit) maxProfit = prices[i] - minPrice;
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+}
